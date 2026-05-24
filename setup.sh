2c9d2d8f21dc6e1abef511124204c43eccc719cf
BiaoZyx <BiaoZyx@outlook.com>
2026-05-24 21:44:13 +0800

    Fixed the setup_shell, and it can work now~

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo Now, the pwd is $pwd, if the script doesn\'t work, please choose the currect project\'s path. 
-if [ sudo -i ]; 
-	sudo cp ./dist/LCT-v1.3.py /usr/local/bin/lct
-	echo Success! 
+echo Now, the pwd is $(pwd), if the script doesn\'t work, please choose the currect project\'s path. 
+if command sudo -v ;then 
+  sudo cp ./dist/LCT-v1.3 /usr/local/bin/lct
+  echo Success! 
 else
-	echo Are you root or a sudoer? 
+  echo Are you root or a sudoer? 
+fi
